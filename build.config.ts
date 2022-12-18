6ab981d90fd85c6804f4b2db385c16972ce5a5c5
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2022-12-18 12:24:24 +0000

    enable source maps

--- build.config.ts
+++ build.config.ts
@@ -4,5 +4,14 @@ export default defineBuildConfig({
   declaration: true,
   rollup: {
     emitCJS: true
+  },
+  hooks: {
+    'rollup:options': (_, options) => {
+      if (!Array.isArray(options.output)) {
+        return;
+      }
+      options.output[0].sourcemap = true;
+      options.output[1].sourcemap = true;
+    }
   }
 });
